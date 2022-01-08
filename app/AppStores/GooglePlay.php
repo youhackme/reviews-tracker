89e46ea5c5f1b7da4174ebee32d318c478bc8a3c
Hyder <hyder@browzzin.com>
2022-01-08 14:16:43 +0400

    Add Guzzle + wip for Apple Store

---
+++ app/AppStores/GooglePlay.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyder Bangash
+ * Date: 1/8/22
+ * Time: 1:22 PM
+ */
+
+namespace App\AppStores;
+
+
+class GooglePlay
+{
+
+}
---
+++ app/StoreInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hyder Bangash
+ * Date: 1/8/22
+ * Time: 1:25 PM
+ */
+
+namespace App;
+
+
+interface StoreInterface
+{
+
+    public function reviews();
+
+    public function ratings();
+
+    public function search();
+
+}
