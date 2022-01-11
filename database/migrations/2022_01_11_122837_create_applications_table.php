46e17c2725c10fbbc3f06993c6f1f1d0fef79b08
Hyder Bangash <hyder@browzzin.com>
2022-01-11 17:45:56 +0400

    Create Initial Migrations

---
+++ database/migrations/2022_01_11_122837_create_applications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('applications_id');
+            $table->string('name', 250);
+            $table->json('screenshots');
+            $table->string('icon', 250);
+            $table->string('developer_url', 250);
+            $table->json('languages');
+            $table->unsignedInteger('reviews');
+            $table->decimal('score', $precision = 2, $scale = 2);
+            $table->string('url', 250);
+            $table->dateTime('released_at');
+            $table->unsignedInteger('developer_id');
+            $table->string('genre', 75);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
